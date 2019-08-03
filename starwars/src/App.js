24a995a6513a0fae705e5885b34d6307c6dfc887
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-08-02 21:23:28 -0600

    struggling with hook calls, no mater what function i put it in, it will pull the data but not pass it

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 
-const App = () => {
   axios.get('https://swapi.co/api/people/1/')
-    .then((response)=>{
-      console.log('this is the data: ',response.data);
+    .then((res)=>{ 
+      console.log('this is the data: ',res.data);
+      console.log('this is the name:',res.data.name);
+      console.log('this is the gender:', res.data.gender);
+      console.log('this is the birth year:',res.data.birth_year);
+      console.log('this is the eye color:',res.data.eye_color);
+      console.log('this is the hair color:',res.data.hair_color);
+
     });
+
    
+      const[data,setData]=useState({});
+      const[name,setName]=useState('');
+      const[gender,setGender]=useState('');
+      const[birthYear,setBirthYear]=useState('');
+      const[eyeColor,setEyeColor]=useState('');
+      const[hairColor,setHairColor]=useState('');
+
+      setData(this.res.data)
+      setName(this.res.data.name)
+      setGender(this.res.data.gender)
+      setBirthYear(this.res.data.birth_year)
+      setEyeColor(this.res.data.eye_color)
+      setHairColor(this.res.data.hair_color)
+    
+ 
+    
+
+
+const App = () => {
+  
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-
+      <div>
+        <h4>Name:{this.name}</h4>
+        <p>Gender:{}</p>
+        <p>Birth Year:{}</p>
+        <p>Eye Color:{}</p>
+        <p>Hair Color:{}</p>
+      </div>
     </div>
   );
 }
